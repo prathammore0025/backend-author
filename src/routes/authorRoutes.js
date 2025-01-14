8ee6d1aa55818bfd576ccb0ffd17430d6c41ce9d
Pratham More <156173075+prathammore0025@users.noreply.github.com>
2025-01-14 12:55:24 +0530

    Update authorRoutes.js

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -3,7 +3,10 @@ const { getAuthors, getAuthorById } = require("../controllers/authorController")
 
 const router = express.Router();
 
+// Routes to fetch authors
 router.get("/", getAuthors);
+
+// Route to fetch a single author by ID
 router.get("/:id", getAuthorById);
 
 module.exports = router;
