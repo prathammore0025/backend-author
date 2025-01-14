0428ce7a9165dfbec4d074605635c0ea4c2750cc
Pratham More <156173075+prathammore0025@users.noreply.github.com>
2025-01-14 12:56:32 +0530

    Update authorModel.js

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 
+// Define the schema for an author
 const authorSchema = new mongoose.Schema({
   name: { type: String, required: true },
   bio: { type: String, required: true },
   photo: { type: String, required: true },
 });
 
+// Create the Author model based on the schema
 const Author = mongoose.model("Author", authorSchema);
 
 module.exports = Author;
